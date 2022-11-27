9c32073e1d08533774d1dede53ad7065d28d3443
Vic GUTT <guttvictor@yahoo.fr>
2022-11-27 14:31:51 +0100

    feat: specify all .js files within are ES modules

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     '*.{js,ts,vue}': ['eslint . --max-warnings=0', 'prettier -l'],
     '*.json': 'eslint . --max-warnings=0',
     '*.{json,html,yml,md,css,php}': 'prettier . -l',
